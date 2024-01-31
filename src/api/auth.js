b43d2206cb7e94a8c4d0daec2cbcc5ced1bdb5f0
Yefferson Bocanegra Lara <ybocanegra@adgasd32.int>
2024-01-31 11:19:04 -0500

    Registro de usuario en Backend Exitoso

---
+++ src/api/auth.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const API = "http://localhost:3400/api";
+
+export const registerRequest = (user) => axios.post(`${API}/registro`, user);
--- src/pages/Registro/RegistroPage.jsx
+++ src/pages/Registro/RegistroPage.jsx
@@ -1,15 +1,25 @@
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
+import { useState } from "react";
+import { registerRequest } from "../../api/auth";
 import './RegistroPage.css'
 import LogoThreeM from "../../assets/img/LogoThreeM.png";
+import eyeClose from "../../assets/icon/eyeClose.svg";
+import eyeOpen from "../../assets/icon/eyeOpen.svg";
 
 function RegistroPage() {
   const { register, handleSubmit } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  }
   
   return (
     <div>
-      <form onSubmit={ handleSubmit (values => {
+      <form onSubmit={ handleSubmit (async (values) => {
         console.log(values);
+        const res = await registerRequest(values);
+        console.log(res);
       })}>
         <div className="Logo">
           <img src={ LogoThreeM } alt="" />
@@ -18,12 +28,31 @@ function RegistroPage() {
         <input type="text" {...register("usuario", { required: true })} placeholder="Ejemplo Three_M" />
         <label htmlFor="">Correo electronico:</label>
         <input type="email" {...register("correo", { required: true })} placeholder="usuario@three_m.com" />
-        <label htmlFor="">Contraseña:</label>
-        <input type="password" {...register("contrasena", { required: true })} placeholder="********" />
+        <label  htmlFor="contrasena">
+        Contraseña:
+          <div className="password-input-container">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              {...register('contrasena', { required: true })}
+              placeholder="********"
+            />
+            <img
+              src={showPassword ? eyeOpen : eyeClose}
+              alt="Toggle Password Visibility"
+              onClick={togglePasswordVisibility}
+              className="eye-icon"
+            />
+          </div>
+        </label>
 
+        <div className="BotonesRegistro">
         <button type="submit">
             Registrar
         </button>
+        <button type="submit">
+            Volver
+        </button>
+        </div>
       </form>
     </div>
   );
